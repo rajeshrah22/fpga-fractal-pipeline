f43541ddeeb47ac5606f16d11ed8251c420cdf9e
Rahul Rajesh <rajeshrah22@gmail.com>
2025-10-24 22:01:17 -0400

    v2, prefered solution

--- pipeline_stage.vhd
+++ pipeline_stage.vhd
@@ -20,38 +20,56 @@ architecture rtl of pipeline_stage is
 	signal a2: ads_sfixed;
 	signal b2: ads_sfixed;
 	signal ab: ads_sfixed;
-begin
-	part1: process(clock, reset) is -- can we instead do this in the falling edge of the clock in the same process?
-	begin
-		if reset = '0' then
-			a2 <= to_ads_sfixed(0);
-			b2 <= to_ads_sfixed(0);
-			ab <= to_ads_sfixed(0);
-		elseif rising_edge(clock) then
-			a2 <= stage_input.z.re * stage_input.z.re;
-			b2 <= stage_input.z.im * stage_input.z.im;
-			ab <= stage_input.z.re * stage_input.z.im;
-		end if;
-	end process part1;
+	signal a2_out: ads_sfixed;
+	signal b2_out: ads_sfixed;
+	signal ab_out: ads_sfixed;
 
-	part2: process(clock, reset) is
+	signal z2plusc: ads_complex;
+	signal abs2z: ads_sfixed;
+	signal z2plusc_out: ads_complex;
+	signal abs2z_out: ads_sfixed;
+begin
+	-- multiplication
+	a2 <= stage_input.z.re * stage_input.z.re;
+	b2 <= stage_input.z.im * stage_input.z.im;
+	ab <= stage_input.z.re * stage_input.z.im;
+
+	mult_reg: process(clock) is
 	begin
-		if reset = '0' then
-			stage_output <= (others => '0');
-		elseif rising_edge(clock) then
-			if stage_input.stage_overflow then
-				stage_output.stage_data <= stage_input.stage_data;
-			else
-				stage_output.stage_data <= stage_input.stage_number;
-			end if;
-			stage_output.c <= stage_input.c;
-			stage_output.z.re <= a2 - b2 + 2 * ab;
-			if (a2 + b2) > threshold then
-				stage_output.stage_overflow <= stage_input.stage_overflow;
-			else
-				stage_output.stage_overflow <= false;
-			end if;
+		if rising_edge(clock) then
+			a2_out <= a2;
+			b2_out <= b2;
+			ab_out <= ab;
 		end if;
-	end process part2;
+	end process mult_reg;
+
+	-- addition
+	z2plusc.re <= a2 - b2 + stage_input.c.re;
+	z2plusc.im <= ab + ab + stage_input.c.im;
+	abs2z <= a2 + b2;
+
+	add_reg: process(clock) is
+	begin
+		if rising_edge(clock) then
+			z2plusc_out <= z2plusc;
+			abs2z_out <= abs2z;
+		end if;
+	end process add_reg;
+
+	-- comparison and output
+	if stage_input.stage_overflow then
+		stage_output.stage_data <= stage_input.stage_data;
+	else
+		stage_output.stage_data <= stage_input.stage_number;
+	end if;
+
+	if abs2z_out > threshold then
+		stage_output.stage_overflow <= stage_input.stage_overflow;
+	else
+		stage_output.stage_overflow <= false;
+	end if;
+
+	stage_output.c <= stage_input.c;
+	stage_output.z <= z2plusc_out;
 end architecture rtl;
 
