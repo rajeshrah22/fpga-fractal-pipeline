3c334b892f18f45c4ae845e478077e8cdb4951e4
Shwinayy <164042247+Shwinayy@users.noreply.github.com>
2025-11-03 18:26:38 -0500

    Refactor fractal_testbench to use test_toplevel

--- fractal_testbench.vhd
+++ fractal_testbench.vhd
@@ -1,3 +1,4 @@
+-- bring in the necessary libraries and packages
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -8,106 +9,110 @@ use std.textio.all;
 library vga;
 use vga.vga_data.all;
 
+library ads;
+use ads.ads_complex_pkg.all;
+use ads.ads_fixed.all;
+
+use work.pipeline_pkg.all;
+use work.color_data.all;
+
 entity write_file_example is
-	generic (
-		clock_period: time := 10 ps
-	);
+    generic (
+        clock_period: time := 10 ps
+    );
 end entity write_file_example;
 
 architecture demo of write_file_example is
-	-- TODO: make this match your own toplevel
-	component fractal_generator is
-		generic (
-			stages: positive := 16;
-			vga_res: vga_data := vga_res_default
-		);
-		port (
-			reset:	in	std_logic;
-			clock:	in	std_logic;
-			f_select:	in	std_logic;
-			h_sync:	out	std_logic;
-			v_sync:	out	std_logic;
-			color:	out	vga_color
-		);
-	end component fractal_generator;
 
-	-- TODO: fix if necessary
-	constant h_res: positive :=
-			vga_res_default.horizontal.active
-		+	vga_res_default.horizontal.front_porch
-		+	vga_res_default.horizontal.sync_width
-		+	vga_res_default.horizontal.back_porch;
-	constant v_res: positive :=
-			vga_res_default.vertical.active
-		+	vga_res_default.vertical.front_porch
-		+	vga_res_default.vertical.sync_width
-		+	vga_res_default.vertical.back_porch;
+    component test_toplevel is
+        generic (
+            stage_count: natural := 16
+        );
+        port (
+            clock: in std_logic;
+            reset: in std_logic;
+            fractal_select: in std_logic;
+            h_sync: out std_logic;
+            v_sync: out std_logic;
+            vga_color: out rgb_color
+        );
+    end component test_toplevel;
 
-	signal clock: std_logic := '0';
-	signal reset: std_logic := '0';
-	signal f_select: std_logic := '0';
+    constant h_res: positive :=
+        vga_res_default.horizontal.active +
+        vga_res_default.horizontal.front_porch +
+        vga_res_default.horizontal.sync_width +
+        vga_res_default.horizontal.back_porch;
 
-	signal h_sync, v_sync: std_logic;
-	signal color: vga_color;
+    constant v_res: positive :=
+        vga_res_default.vertical.active +
+        vga_res_default.vertical.front_porch +
+        vga_res_default.vertical.sync_width +
+        vga_res_default.vertical.back_porch;
 
-	signal test_done: boolean := false;
+    signal clock: std_logic := '0';
+    signal reset: std_logic := '0';
+    signal fractal_select: std_logic := '0';
+
+    signal h_sync, v_sync: std_logic;
+    signal vga_color: rgb_color;
+
+    signal test_done: boolean := false;
+
 begin
 
-	clock <= not clock after clock_period / 2 when not test_done else '0';
+    clock <= not clock after clock_period / 2 when not test_done else '0';
 
-	-- TODO: fix if necessary
-	dut: fractal_generator
-		generic map (
-			stages =>	16,
-			vga_res => vga_res_default
-		)
-		port map (
-			reset =>	reset,
-			clock =>	clock,
-			f_select => '1',
-			h_sync =>	h_sync,
-			v_sync =>	v_sync,
-			color =>	color
-		);
+    dut: test_toplevel
+        generic map (
+            stage_count => 16
+        )
+        port map (
+            clock => clock,
+            reset => reset,
+            fractal_select => '1',
+            h_sync => h_sync,
+            v_sync => v_sync,
+            vga_color => vga_color
+        );
 
-	write_file: process is
-		file out_file: text open write_mode is "my_generated_file.ppm";
-		variable out_line: line;
-		variable r, g, b: natural range 0 to 15;
-	begin
-		write(out_file, "P3" & lf);
-		write(out_file, integer'image(h_res) & " " &
-					integer'image(v_res) & lf);
-		write(out_file, "15" & lf);
+    write_file: process is
+        file out_file: text open write_mode is "my_generated_file.ppm";
+        variable out_line: line;
+        variable r, g, b: natural range 0 to 15;
+    begin
+        write(out_file, "P3" & lf);
+        write(out_file, integer'image(h_res) & " " &
+                        integer'image(v_res) & lf);
+        write(out_file, "15" & lf);
 
-		reset <= '0';
-		wait for rising_edge(clock);
-		wait for rising_edge(clock);
-		reset <= '1';
+        reset <= '0';
+        wait for rising_edge(clock);
+        wait for rising_edge(clock);
+        reset <= '1';
 
-		for i in 0 to 15 loop
-			wait for rising_edge(clock);
-		end loop;
+        for i in 0 to 15 loop
+            wait for rising_edge(clock);
+        end loop;
 
-		-- TODO: check; this _should_ work
-		for j in 0 to v_res - 1 loop
-			for i in 0 to h_res - 1 loop
-				r := color.red;
-				g := color.green;
-				b := color.blue;
+        for j in 0 to v_res - 1 loop
+            for i in 0 to h_res - 1 loop
+                r := vga_color.red;
+                g := vga_color.green;
+                b := vga_color.blue;
 
-				write(out_line,
-						integer'image(r) & " " &
-						integer'image(g) & " " &
-						integer'image(b) );
-				writeline(out_file, out_line);
+                write(out_line,
+                        integer'image(r) & " " &
+                        integer'image(g) & " " &
+                        integer'image(b));
+                writeline(out_file, out_line);
 
-				wait for rising_edge(clock);
-			end loop;
-		end loop;
+                wait for rising_edge(clock);
+            end loop;
+        end loop;
 
-		test_done <= true;
-		wait;
-	end process write_file;
+        test_done <= true;
+        wait;
+    end process write_file;
 
 end architecture demo;
